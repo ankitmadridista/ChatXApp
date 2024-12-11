46ec87ce6bf8bc2f7021d12c359ef85192ad3d8b
ankitmadridista <ankit.suravkar4@gmail.com>
2024-12-11 21:23:51 +0530

    register-login-functionality-added

--- public/src/App.js
+++ public/src/App.js
@@ -1,18 +1,18 @@
-import react from "react";
-import {BrowserRouter, Routes, Route} from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Chat from "./pages/Chat";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import SetAvatar from "./pages/SetAvatar";
 
-export default function App(){
+export default function App() {
   return (
-      <BrowserRouter>
+    <BrowserRouter>
       <Routes>
-          <Route path = '/register' element = {<Register />}/>
-          <Route path = '/login' element = {<Login />}/>
-          <Route path = '/' element = {<Chat />}/>
+        <Route path='/register' element={<Register />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/' element={<Chat />} />
       </Routes>
-      </BrowserRouter>
+    </BrowserRouter>
   );
 
 }
